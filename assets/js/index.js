83e5286d9e1bdab07154643ec177766f17cff649
Ahmad Youssef <ahmad.youssef@edatasystems.de>
2024-03-25 12:15:26 +0100

    Initial Project

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,180 @@
-import {Data, DataContainer,} from '../data/index';
+export const DataContainer = {
+  class: '',
+  rows: 13,
+  cols: 4,
+  width: '48%',
+  height: 'calc(100vh - 50px - 100px)'
+};
+
+export const Data = [
+  {
+    'id': 'item-1',
+    'class': '',
+    'row': 3,
+    'col': 4,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-2',
+    'class': '',
+    'row': 1,
+    'col': 3,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-3',
+    'class': '',
+    'row': 1,
+    'col': 1,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-4',
+    'class': '',
+    'row': 2,
+    'col': 1,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-5',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-6',
+    'class': '',
+    'row': 2,
+    'col': 1,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-7',
+    'class': '',
+    'row': 1,
+    'col': 2,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-8',
+    'class': '',
+    'row': 1,
+    'col': 1,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-9',
+    'class': '',
+    'row': 1,
+    'col': 1,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-10',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [{
+      src: './assets/images/image_10.jpeg',
+      alt: 'logo',
+      width: "100%",
+      height: "100%",
+    }],
+    'style': ''
+  },
+  {
+    'id': 'item-11',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [{
+      src: './assets/images/image_11.jpeg',
+      alt: 'logo',
+      width: "100%",
+      height: "100%",
+    }],
+    'style': ''
+  },
+  {
+    'id': 'item-12',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [
+      {
+        class: "sliderImage",
+        src: './assets/images/image_12-1.jpeg',
+        alt: 'logo',
+        width: "100%",
+        height: "100%",
+      },
+      {
+        class: "sliderImage",
+        src: './assets/images/image_12-2.jpeg',
+        alt: 'logo',
+        width: "100%",
+        height: "100%",
+      },
+      {
+        class: "sliderImage",
+        src: './assets/images/image_12-3.jpeg',
+        alt: 'logo',
+        width: "100%",
+        height: "100%",
+      }
+    ],
+    'style': ''
+  },
+  {
+    'id': 'item-13',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [{
+      class: "",
+      src: './assets/images/image_13.jpeg',
+      alt: 'logo',
+      width: "100%",
+      height: "100%",
+    }],
+    'style': ''
+  },
+  {
+    'id': 'item-14',
+    'class': '',
+    'row': 2,
+    'col': 2,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-15',
+    'class': '',
+    'row': 1,
+    'col': 2,
+    'images': [],
+    'style': ''
+  },
+  {
+    'id': 'item-16',
+    'class': '',
+    'row': 1,
+    'col': 2,
+    'images': [],
+    'style': ''
+  }
+];
+
 
 (function() {
   const mainDom = document.querySelector("#main");
